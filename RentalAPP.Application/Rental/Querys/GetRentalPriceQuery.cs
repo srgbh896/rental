ce37ddd8981f35524075285ed9f60b9a2b70b470
Sergio <sergiobarrado25@gmail.com>
2025-04-14 08:42:42 +0200

    Fixed rental calc

--- RentalAPP.Application/Rental/Querys/GetRentalPriceQuery.cs
+++ RentalAPP.Application/Rental/Querys/GetRentalPriceQuery.cs
@@ -13,19 +13,20 @@ public record GetRentalPriceQuery() : IRequest<RentalPriceDto>
     public DateTime EndDate { get; set; }
 }
 
-public class GetRentalPriceQueryHandler(ICarRepository carRepository, IMapper mapper) : IRequestHandler<GetRentalPriceQuery, RentalPriceDto>
+public class GetRentalPriceQueryHandler(ICarRepository carRepository) : IRequestHandler<GetRentalPriceQuery, RentalPriceDto>
 {
     private readonly ICarRepository _carRepository = carRepository;
-    private readonly IMapper _mapper = mapper;
 
     public async Task<RentalPriceDto> Handle(GetRentalPriceQuery request, CancellationToken cancellationToken)
     {
         var totalDays = (int)(request.EndDate - request.StartDate).TotalDays;
+        if (totalDays <= 0) throw new Exception("Rental must be at least one day."); 
         RentalPriceDto result = new();
-        var car = await _carRepository.GetByIdAsync(request.CarId);
-
+        var car = await _carRepository.GetByIdAsync(request.CarId)
+          ?? throw new Exception("Car not found");
         result.TotalPrice = RentalDomainService.CalculateRentalPrice(car, totalDays);
         result.PricePerDay = result.TotalPrice / totalDays;
+        result.Days = totalDays;
         return result;
     }
 }
